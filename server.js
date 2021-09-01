549750e344a1a4bb737108876341451abd0bb053
racine-p-a <pierrealexandreracine@gmail.com>
2021-09-01 13:26:49 +0200

    Kepler default configuration management.

--- server.js
+++ server.js
@@ -2,34 +2,53 @@
  * Server entrypoint.
  */
 
-
 /*
     Configs.
     TODO : put those configs in the env variables for deployments.
  */
 // const port
 
-
 /*
     Imports
  */
-const express = require('express'); // Webserver
-let app = express();
+const express = require('express') // Webserver
+let app = express()
 
-const KeplerConfiguration = require('./src/KeplerConfiguration');
-const DataSources = require('./src/DataSources');
+const KeplerConfiguration = require('./src/KeplerConfiguration')
+const DataSources = require('./src/DataSources')
+
+app.use(express.json())
+
+const has = Object.prototype.hasOwnProperty;
 
 /*
     Routes
-    /api/conf/      -> request for default configuration    -> send back the recorded Kepler configuration
-    /?todo          -> request for storing desired modifications/updates of the Kepler configuration
-    /api/data/${nomData}    -> request to get the data of a dataset (in a geojson Kepler style)
+    /api/conf/        -> request for default configuration                                    -> send back the recorded Kepler configuration
+    /api/conf/(POST)  -> request for storing a new default configuration (new_configuration)  -> send back the recorded Kepler configuration
+    /api/data/${Datum}-> request to get the data of a dataset (in a geojson Kepler style)
  */
 
 //  /api/conf
-app.get('/api/conf/', (req, res) => {
-    res.send({ "data":DataSources.getDataSources(), "KeplerConfiguration":KeplerConfiguration.getConfiguration()});
-}); // TODO : test presence of a POST variable (an authorization token)
+app.post('/api/conf/', (req, res) => {
+  if (has.call(req.query, "new_configuration")){
+    KeplerConfiguration.storeConfiguration(req.query.new_configuration)
+  } else {
+    res.send({ "data":DataSources.getDataSources(), "KeplerConfiguration":KeplerConfiguration.getConfiguration()})
+  }
+});
 
+app.post('/api/users', function(req, res) {
+  /*
+  const user_id = req.body.id;
+  const token = req.body.token;
+  const geo = req.body.geo;
+
+  res.send({
+      'user_id': user_id,
+      'token': token,
+      'geo': geo
+  });
+  */
+});
 
 app.listen(3000); // todo port in env variable
--- src/KeplerConfiguration.js
+++ src/KeplerConfiguration.js
@@ -2,25 +2,24 @@
  * Management of the Kepler configuration.
  */
 
-const fs = require('fs');
+const fs = require('fs')
 
 /**
  * Reads the Kepler configuration file and sends its content back.
  * @returns {string}
  */
 module.exports.getConfiguration = () => {
-    return readConfFile('data/KeplerConfiguration.json');
-};
-
+  return readConfFile('data/KeplerConfiguration.json')
+}
 
 module.exports.storeConfiguration = (content) => {
-    fs.writeFile('data/KeplerConfiguration.json', content, err => {
-        if (err) {
-            console.error(err)
-        }
-    });
-};
+  fs.writeFile('data/KeplerConfiguration.json', content, err => {
+    if (err) {
+      console.error(err)
+    }
+  })
+}
 
 /*                                          METHODS                                                                   */
 
-const readConfFile = (configFile) => {return  fs.readFileSync(configFile, 'utf8');};
+const readConfFile = (configFile) => { return fs.readFileSync(configFile, 'utf8') }
