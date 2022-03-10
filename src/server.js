fad2253d4506fd97d3a0328d1bd8698d297c6264
p-a-racine <pierrealexandreracine@gmail.com>
2022-03-10 15:09:23 +0100

    feat : POSTs are now effective again

--- src/server.js
+++ src/server.js
@@ -1,83 +1,50 @@
-/**
- * Server entrypoint.
- */
-
-/*
-    Imports
- */
-const multer  = require('multer')
-const express = require('express') // Webserver
-let cors = require('cors')
-const upload = multer({ dest: 'uploads/' }).single('image')
-
-let app = express()
-app.use(cors())
-
+// IMPORTS
+const express = require('express');
+const formidableMiddleware = require('express-formidable');
 require('dotenv').config();
-
-
 const KeplerConfiguration = require('./KeplerConfiguration')
-const DataSources = require('./DataSources')
-//app.use(upload.array());
-app.use(express.static('public'));
-
 const DataNotion = require('./src/helpers/notion')
 const DataWordpress = require('./src/helpers/wordpress')
 
+// SERVER CONFIGURATION
+const app = express();
+app.listen(process.env.DATATLAS_BACK_END_PORT);
+console.log("Listening on this port :", process.env.DATATLAS_BACK_END_PORT)
+app.use(formidableMiddleware());
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-
+// INTERNAL TOOLS
 const has = Object.prototype.hasOwnProperty;
 
-/*
-    Routes
-GET   /api/test/          -> request to check connection                                          -> send back « test »                     DONE
-GET   /api/conf/:confWanted/ -> request for a conf data.                                                                                          DONE
-POST  /api/conf/:confWanted/ -> request for storing a conf data.                                                                                  DONE
-POST  /api/upload/            -> request to store a file              -> NOT WORKING
+// ROUTES
+app.route('/api/test/')
+  .get(function(req, res) {
+    res.status(200).send('test')
+  });
 
-    /api/conf/retrieve/ -> request for default configuration                                    -> send back the recorded Kepler configuration
-    /api/conf/store/    -> request for storing a new default configuration (new_configuration)  -> send back the two recorded Kepler configuration
-    /api/data/${DataType}/${Datum}  -> request to get the data of a dataset (in a geojson Kepler style)
- */
-
-//  /api/test
-app.get('/api/test/', (req, res, next) => {
-  res.send('test')
-  next();
-});
-
-
-//  /api/conf/:confWanted/
 // todo : check security : anyone can send data...
-app.get('/api/conf/:confWanted/', function (req, res, next) {
+app.get('/api/conf/:confWanted/', (req, res) => {
   if (req.params.confWanted === 'kepler') {
-    res.send(KeplerConfiguration.getKeplerConfiguration())
+    res.status(200).send(KeplerConfiguration.getKeplerConfiguration())
   } else if (req.params.confWanted === 'instance') {
-    res.send( KeplerConfiguration.getLayersConfiguration())
+    res.status(200).send( KeplerConfiguration.getLayersConfiguration())
   } else {
-    res.send('Unknown conf.');
+    res.send('Unknown conf.'); // todo place a proper status code
   }
-  next();
 });
-
-
-//  /api/conf/store
 // todo : check security : anyone can send data...
-app.post('/api/conf/:confWanted/', (req, res, next) => {
-  if (req.params.confWanted === 'kepler' && has.call(req.body, 'configuration_kepler')){
-    KeplerConfiguration.storeConfigurationKepler(req.body.configuration_kepler)
-    res.send();
-  } else if (req.params.confWanted === 'instance' && has.call(req.body, 'configuration_instance')){
-    KeplerConfiguration.storeConfigurationLayers(req.body.configuration_instance)
-    res.send();
+app.post('/api/conf/:confWanted/', (req, res) => {
+  if (req.params.confWanted === 'kepler' && has.call(req.fields, 'configuration_kepler')){
+    KeplerConfiguration.storeConfigurationKepler(req.fields.configuration_kepler)
+    res.status(200).send();
+  } else if (req.params.confWanted === 'instance' && has.call(req.fields, 'configuration_instance')){
+    KeplerConfiguration.storeConfigurationLayers(req.fields.configuration_instance)
+    res.status(200).send();
   } else{
-    res.send('unknown data');
+    res.send('unknown data'); // todo place a proper status code
   }
 });
 
-//  /api/data/${DataType}/${Datum}
+// todo : check security : anyone can send data...
 app.get('/api/data/:dataType/:dataWanted/', function (req, res, next) {
   if (req.params.dataType === 'notion') {
     DataNotion.notionRequest(req.params.dataWanted).then(function (response) {
@@ -87,22 +54,12 @@ app.get('/api/data/:dataType/:dataWanted/', function (req, res, next) {
     DataWordpress.wordpressRequest(req.params.dataWanted).then(function (response) {
       res.send(DataWordpress.toGeoJson(response))
     })
-
-
   }
 });
 
 
-/*app.post('/api/upload', upload.single('image'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-
-  // DOES NOT WORK ! req.file is ALWAYS undefined whatever i do.
-  console.log(req.file)
-  console.log(req.body)
-
-})*/
 
 
-app.listen(process.env.DATATLAS_BACK_END_PORT);
-console.log("Server started on port :", process.env.DATATLAS_BACK_END_PORT )
+
+
+
